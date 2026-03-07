b9d93901475f165bd6233dc1e14d493abea4f2ac
Abhay Sinha <161612516+AbhaySinha1909@users.noreply.github.com>
2026-03-07 15:52:29 +0530

    Remove Rasa startup commands from start.sh
    Removed Rasa startup commands from the script.

--- start.sh
+++ start.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-echo "Starting Rasa..."
-rasa run \
-  --model models/20260307-122807-tranquil-cricket.tar.gz \
-  --enable-api \
-  --cors "*" \
-  --port 5005 &
-
-sleep 20
-
-echo "Starting Flask..."
 gunicorn app:app --bind 0.0.0.0:$PORT
