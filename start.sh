4d8ff5a0bf94e7a01c7c22843a319f8107b37730
Abhay Sinha <161612516+AbhaySinha1909@users.noreply.github.com>
2026-03-07 15:25:27 +0530

    Update Rasa run command and sleep duration

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 echo "Starting Rasa..."
-rasa run --enable-api --cors "*" --port 5005 &
+rasa run \
+  --model models/20260307-122807-tranquil-cricket.tar.gz \
+  --enable-api \
+  --cors "*" \
+  --port 5005 &
 
-sleep 15
+sleep 20
 
 echo "Starting Flask..."
 gunicorn app:app --bind 0.0.0.0:$PORT
